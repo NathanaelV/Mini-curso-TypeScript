e829927cb335f4f16fb49a3b80cfc4f52b00f4ad
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-02-22 19:33:38 -0300

    Add index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
-const button = document.getElementById("button");
-
+// Há a necessidade de explicitar que o input 1 e o input 2 são váriaveis que vêm do HTML
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+var button = document.getElementById("button");
+// Não há a necessidade de explicitar o tipo que a função irá retornar, pois o TS já sabe que number + number retorna um nubmer
 function sum(a, b) {
-  return a + b;
+    return a + b;
 }
-
-button.addEventListener("click", function() {
-  console.log(sum(input1.value, input2.value));
+button.addEventListener("click", function () {
+    console.log(sum(Number(input1.value), Number(input2.value)));
 });
